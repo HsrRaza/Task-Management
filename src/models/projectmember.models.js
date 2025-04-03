735a41f82bb39f29d679dc5e57a86e4b18b6d18f
Hassan Raza <hr.raza04@gmail.com>
2025-04-03 20:00:29 +0530

    data modeling

--- src/models/projectmember.models.js
+++ src/models/projectmember.models.js
@@ -1,6 +1,24 @@
 import mongoose, { Schema } from "mongoose";
+import { AvailableUserRoles } from "../utils/constants.js"
 
-const projectMemberSchema = new Schema({})
+const projectMemberSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    projects: {
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+        required: true
+    },
+    role: {
+        type: String,
+        enum: AvailableUserRoles,
+        default: UserRolesEnum.MEMBER,
+    }
+
+}, { timestamps: true })
 
 
 export const ProjectMember = mongoose.model("ProjectMember", projectMemberSchema);
--- src/models/task.models.js
+++ src/models/task.models.js
@@ -1,6 +1,32 @@
 import mongoose, { Schema } from "mongoose";
 
-const taskSchema = new Schema({})
+const taskSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    descriptiom: {
+        type: String
+    },
+    project: {
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+        required: true,
+    },
+    assignedTo: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    assignedBy:{
+        type: Schema.Types.ObjectId,
+        ref: "User", 
+        required:true,
+    }
+
+
+}, {timestamps:true})
 
 
 export const Task = mongoose.model("Task", taskSchema);
