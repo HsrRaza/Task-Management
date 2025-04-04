a5b1ef4899c99c7f7a6667f9258a9824fffe0c3f
Hassan Raza <hr.raza04@gmail.com>
2025-04-04 11:27:35 +0530

    routes and controller setup

--- src/app.js
+++ src/app.js
@@ -2,4 +2,9 @@ import express from "express";
 
 const app = express();
 
+// router  imports
+import healhCheckRouter from "./routes/healthcheck.routes.js"
+
+app.use("api/v1/healthcheck",healhCheckRouter)
+
 export default app;
