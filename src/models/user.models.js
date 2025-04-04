c288b89ad8db6e4d3acd3d77c45a28560aa71b40
Hassan Raza <hr.raza04@gmail.com>
2025-04-04 11:13:43 +0530

    Done hooks methods

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,7 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import crypto from "crypto"
 
 const userSchema = new Schema({
     avatar: {
@@ -17,7 +20,7 @@ const userSchema = new Schema({
         unique: true,
         lowercase: true,
         trim: true,
-        
+
     },
     email: {
         type: String,
@@ -28,10 +31,10 @@ const userSchema = new Schema({
     },
     fullname: {
         type: String,
-        required:true,
+        required: true,
     },
     password: {
-        type:String,
+        type: String,
         required: [true, "Password is required"],
     },
     isEmailVerified: {
@@ -46,17 +49,65 @@ const userSchema = new Schema({
         type: Date,
     },
     refreshToken: {
-        type:String,
+        type: String,
     },
     emailVerificationToken: {
-        type:String,
+        type: String,
     },
     emailVerificationExpiry: {
-        type:Date,
+        type: Date,
     },
 
 
-},{timestamps:true})
+}, { timestamps: true });
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    )
+}
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    )
+}
+
+userSchema.methods, generateTemporaryToken = function () {
+    const unHashedToken = crypto.randomBytes(20).toString("hex")
+
+    const hashedToken = crypto
+        .createHash("sha256")
+        .update(unHashedToken).digest('hex')
+
+    const tokenExpiry = Date.now() + (20 * 60 * 1000) // 20min
+
+    return {hashedToken, unHashedToken, tokenExpiry}
+}
+
 
 
 export const User = mongoose.model("User", userSchema);
