bf91e901cb6d7a967419b93664b88a4a1ad75679
Hassan Raza <hr.raza04@gmail.com>
2025-04-06 19:49:59 +0530

    intializing controllers

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -5,8 +5,57 @@ const registerUser = asyncHandler( async(req, res)=> {
 
  
     // Validation
-    registerValidation(body)
-
 })
+    
+const loginUser = asyncHandler( async(req, res)=> {
+    const {emai, username, passsword, role} = req.body;
+
+ 
+    // Validation
+})
+const logoutUser = asyncHandler( async(req, res)=> {
+    const {emai, username, passsword, role} = req.body;
+
+ 
+    // Validation
+})
+const verifyEmail = asyncHandler( async(req, res)=> {
+    const {emai, username, passsword, role} = req.body;
+
+ 
+    // Validation
+})
+const resendVerificationEmail = asyncHandler( async(req, res)=> {
+    const {emai, username, passsword, role} = req.body;
+
+ 
+    // Validation
+})
+const refreshAcessToken = asyncHandler( async(req, res)=> {
+    const {emai, username, passsword, role} = req.body;
+
+ 
+    // Validation
+})
+const forgetPasswordRequest  = asyncHandler( async(req, res)=> {
+    const {emai, username, passsword, role} = req.body;
+
+ 
+    // Validation
+})
+const changeCureentPassword  = asyncHandler( async(req, res)=> {
+    const {emai, username, passsword, role} = req.body;
+
+ 
+    // Validation
+})
+const getCurrentUser  = asyncHandler( async(req, res)=> {
+    const {emai, username, passsword, role} = req.body;
+
+ 
+    // Validation
+})
+    
+
 
 export {registerUser}
