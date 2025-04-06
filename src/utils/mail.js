ace7f65ba5db06dceaeee566fc095d5aa14dda7f
Hassan Raza <hr.raza04@gmail.com>
2025-04-06 18:19:20 +0530

    multer npm done

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -10,6 +10,35 @@ const sendMail = async(options)=>{
             link: 'https://mailgen.js/',
         }
     });
+    var emailText = mailGenerator.generatePlaintext(options.mailGenContent)
+    var emailHtml = mailGenerator.generate(options.mailGenContent)
+
+    const transporter = nodemailer.createTransport({
+        host:process.env.MAILTRAP_SMTP_HOST,
+        port: 587,
+        secure: false, // true for port 465, false for other ports
+        auth: {
+          user: process.env.MAILTRAP_SMTP_USER,
+          pass: process.env.MAILTRAP_SMTP_PASS,
+        },
+      });
+
+
+      const mail ={
+        from: "mail.taskmanager@example.com",
+        to: options.email,
+        subject:emailText, // plain text
+        html:emailHtml, // html body
+      };
+
+      try {
+        await transporter.sendMail(mail)
+      } catch (error) {
+        console.error();
+        
+      }
+    
+    
 }
 
 const emailVerificationMailGenContent = (username, verificationUrl) =>{
@@ -23,6 +52,23 @@ const emailVerificationMailGenContent = (username, verificationUrl) =>{
                     color: "#22BC66",
                     text:"Verify your email",
                     link: verificationUrl,
+                }
+            },
+             outro: 'Need help, or have questions? Just reply to this email, we\'d love to help.'
+        }
+    }
+}
+const forgotPasswordMailGenContent = (username, passwordResetUrl) =>{
+    return {
+        body: {
+            name: username,
+            intro:" We got a request to reset your password",
+            action: {
+                instruction : "To change your password click the button",
+                buttion: {
+                    color: "#22BC66",
+                    text:"reset password",
+                    link: passwordResetUrl,
                 }
             },
              outro: 'Need help, or have questions? Just reply to this email, we\'d love to help.'
