5e26046e0562f0f509a8cd01c0140bcbc60ae7d6
Hassan Raza <hr.raza04@gmail.com>
2025-04-13 10:19:21 +0530

    touch up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
 import app from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import cookieParser from "cookie-parser";
+import express from "express"
+
 
 dotenv.config({
     path: "./.env"
 })
 const PORT = process.env.PORT || 8000
+
+// 1. Cookie Parser first
+app.use(cookieParser());
+
+
+// cors configuration
+
+app.use(cors({
+    origin: "http://localhost:5173", // match your frontend URL exactly
+    Credential: true,
+    methods: ['GET', 'POST', 'PUT' , 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    exposedHeaders:['Set-Cookie', '*']
+}));
+
+
+// othermiddlewares
+app.use(express.json());
+app.use(express.urlencoded({extends: true}));
+// app.use(express.static("public"))
+
+
 
 connectDB()
    .then( ()=>{
